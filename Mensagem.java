0b0a68338fd14f7d489144fbb3f63cb1d20c1873
ThiagoH03 <thiagohenriq03@outlook.com>
2022-02-21 19:14:11 +0000

    Mensagem de idade

--- Mensagem.java
+++ Mensagem.java
@@ -1,5 +1,8 @@
 public class Mensagem{
     public static void main(String[] args) {
         System.out.println("Fatec Pompeia");
+        int idade = 19;
+
+        System.out.println("A minha idade é de " + idade +" anos");
     }
 }
